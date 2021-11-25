9131038070ebfb1e85211957aa9ef4b35b677625
Farhan Tanvir <farhan@aleshatech.com.bd>
2021-11-26 02:42:59 +0600

    button event class component

--- src/component/Hello.js
+++ src/component/Hello.js
@@ -1,12 +1,12 @@
 import React from 'react';
 
 function DoThis(){
-    alert('Button click');
+    alert('Button click from functional component');
 }
 function Hello(props){
     return (
         <div>
-            <button onClick={DoThis}>Primary</button>
+            <button onClick={DoThis}>Click me from functional componet</button>
             <h1>Hello, I am from functional component and my name is: {props.name}</h1>
         </div>
         
--- src/component/Welcome.js
+++ src/component/Welcome.js
@@ -1,8 +1,15 @@
 import React,{Component} from 'react';
 
+
 class Welcome extends Component {
+    doThis(){
+        alert('Button click me from class component');
+    }
     render(){
-       return <h1>Hello, I am from class component and my name is: {this.props.name}</h1>
+       return <div>
+                <button onClick={this.doThis}>Click me from class componet</button>
+                <h1>Hello, I am from class component and my name is: {this.props.name}</h1>
+           </div>
     }
 }
 
