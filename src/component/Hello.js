7605a33afff4bab4c9255c89d912eab15bfb73f4
Farhan Tanvir <farhan@aleshatech.com.bd>
2021-11-26 15:46:37 +0600

    param pass

--- src/component/Hello.js
+++ src/component/Hello.js
@@ -1,12 +1,12 @@
 import React from 'react';
 
-function DoThis(){
-    alert('Button click from functional component');
+function DoThis(a){
+    alert(a);
 }
 function Hello(props){
     return (
         <div>
-            <button onClick={DoThis}>Click me from functional componet</button>
+            <button onClick={DoThis.bind(this, "Hello, I am from functional component pram...")}>Click me from functional componet</button>
             <h1>Hello, I am from functional component and my name is: {props.name}</h1>
         </div>
         
